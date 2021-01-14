33a14f443db6810a1eb165031a9467c608aae789
TheMartonfi <carlmartonfi@hotmail.com>
2021-01-13 17:48:49 -0700

    Init discord bot

--- build/index.js
+++ build/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("hello world!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
+const discord_1 = require("@typeit/discord");
+dotenv_1.default.config();
+const { TOKEN } = process.env;
+const start = async () => {
+    const client = new discord_1.Client({
+        classes: [
+            `${__dirname}/*Discord.ts`,
+            `${__dirname}/*Discord.js` // If you compile using "tsc" the file extension change to .js
+        ],
+        silent: false,
+        variablesChar: ":"
+    });
+    await client.login(TOKEN);
+};
+start();
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world!");
+import dotenv from "dotenv";
+import { Client } from "@typeit/discord";
+
+dotenv.config();
+const { TOKEN } = process.env;
+
+const start = async () => {
+	const client = new Client({
+		classes: [
+			`${__dirname}/*Discord.ts`, // glob string to load the classes
+			`${__dirname}/*Discord.js` // If you compile using "tsc" the file extension change to .js
+		],
+		silent: false,
+		variablesChar: ":"
+	});
+
+	await client.login(TOKEN);
+};
+
+start();
